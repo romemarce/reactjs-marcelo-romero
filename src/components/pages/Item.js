d06088f450fd89d8d9123320a727d2d107f80b5e
Marcelo <romemarce1@gmail.com>
2022-05-17 09:07:40 -0300

    romerotech-romero

--- src/components/pages/Item.js
+++ src/components/pages/Item.js
@@ -2,7 +2,7 @@ const Item = ({title, pictureUrl, price}) => {
   return <article className="card">
     <div className="card-image">
       <figure className="image is-4by3">
-        <img src={pictureUrl} alt={title} />
+        <img src={pictureUrl} alt={title}/>
       </figure>
     </div>
     <div className="card-content">
